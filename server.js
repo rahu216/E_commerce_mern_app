2cd1fac8e6eefceaab25198d213c8a986d23c4b0
rahu216 <121561068+rahu216@users.noreply.github.com>
2023-09-29 03:29:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-
+import {fileURLToPath} from 'url';
 dotenv.config();
 
 //rest object
@@ -15,6 +15,8 @@ const app = express();
 
 //database config
 connectdb();
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //middlewares
 app.use(cors());
